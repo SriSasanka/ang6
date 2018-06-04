0b3e4997f2d25556205c06c779c4ec23b7e76cc2
dracula <srisasanka.is@gmail.com>
2018-06-04 07:56:25 +0530

    committed on 04-06-2018 07:56

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  login = {
+    username: '',
+    password: ''
+  };
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  onSubmit(form: NgForm) {
+    console.log('Form Submitted');
+    console.log(form);
+    this.router.navigate(['/anime/home']);
+  }
 }
